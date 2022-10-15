8f9017dc0c10bacf7692cdc5f13369117ac4feda
Fermain <Fermain@users.noreply.github.com>
2022-10-15 19:42:31 +0200

    Adds jest

--- src/js/joker.test.js
+++ src/js/joker.test.js
@@ -1,11 +1,15 @@
-import { dadJoke } from './joker.js';
+// import { dadJoke } from './joker.js';
 
-export async function testDadJoke() {
-  const joke = await dadJoke();
-  const testPasses = Boolean(joke);
-  console.log(joke);
-  console.log('Dad Joke Test:', testPasses ? 'PASSED' : 'FAILED');
-  return testPasses;
-}
+// export async function testDadJoke() {
+//   const joke = await dadJoke();
+//   const testPasses = Boolean(joke);
+//   console.log(joke);
+//   console.log('Dad Joke Test:', testPasses ? 'PASSED' : 'FAILED');
+//   return testPasses;
+// }
 
-testDadJoke();
+// testDadJoke();
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(1 + 2).toBe(3);
+});
